f655f28e3ea1aabff9eadfed596826ece1bcee9d
LeninMiranda <lenin9073@gmail.com>
2026-06-23 18:09:04 -0700

    Global Pipes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { Logger } from "@nestjs/common";
+import { Logger, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   // CORS para que el frontend de Next.js pueda llamar a la API
   app.enableCors({
